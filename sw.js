1b296d44255873ec03569cbafbe2ef7e788613ca
centiger <centiger2@gmail.com>
2026-07-05 13:17:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE='cen-tabernacle-v0.5.3';
-const ASSETS=['./','./index.html','./manifest.json','./scene-overview.jpg','./scene-laver.jpg','./scene-altar.jpg','./scene-sanctuary.jpg','./scene-ark.jpg','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE_NAME = 'cen-tabernacle-v0.6.0';
+const ASSETS = [
+  './','./index.html','./manifest.json',
+  './scene-overview.jpg','./scene-laver.jpg','./scene-altar.jpg','./scene-sanctuary.jpg','./scene-ark.jpg',
+  './assets/icons/icon-192.png','./assets/icons/icon-512.png'
+];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
