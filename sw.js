0595fa31987f3c84bb343c92366df3bbcfad7321
centiger <centiger2@gmail.com>
2026-07-05 12:25:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,5 @@
-const CACHE_NAME = 'cen-tabernacle-v0.3.0';
-const ASSETS = [
-  './','./index.html','./style.css','./app.js','./manifest.json','./db/data.js',
-  './assets/icons/icon-192.png','./assets/icons/icon-512.png',
-  './assets/images/hero-tabernacle.jpg','./assets/images/card-tabernacle.jpg','./assets/images/card-solomon.jpg','./assets/images/card-zerubbabel.jpg','./assets/images/card-herod.jpg','./assets/images/flow-bg.jpg','./assets/images/stage-tabernacle.jpg','./assets/images/stage-temple.jpg'
-];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch', e => {
-  const req = e.request;
-  e.respondWith(fetch(req).then(res => {
-    const copy = res.clone();
-    caches.open(CACHE_NAME).then(c => c.put(req, copy)).catch(()=>{});
-    return res;
-  }).catch(() => caches.match(req).then(r => r || caches.match('./index.html'))));
-});
+const CACHE='cen-tabernacle-v0.4.0';
+const ASSETS=['./','./index.html','./manifest.json','./sw.js','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
