ced76a46a0676ff70e018783c268efc45b853c1a
centiger <centiger2@gmail.com>
2026-07-05 16:48:11 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cen-tabernacle-v1-0-2';
+const CACHE='cen-tabernacle-v1-0-3';
 const ASSETS=['./','./index.html','./style.css','./manifest.json','./data/scenes.js','./js/explorer.js','./scene/scene-overview.jpg','./scene/scene-entrance.jpg','./scene/scene-altar.jpg','./scene/scene-laver.jpg','./scene/scene-sanctuary.jpg','./scene/scene-ark.jpg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
